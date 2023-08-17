f2fe0f9467e01d550ba1a7a10323e1284b7e47c4
IMBensonQB <b10705047@g.ntu.edu.tw>
2023-08-17 15:04:14 +0800

    feat(app/store): edit status, basic info and menu

---
+++ cafe-near-u/app/components/storeside/HeaderStore.jsx
@@ -0,0 +1,16 @@
+import { Button } from "../MaterialTailwind";
+
+export default function HeaderStore() {
+  return (
+    <div className="border-b ">
+      <div className="lg:container lg:mx-auto flex justify-between sticky top-0">
+        <h1 className="text-3xl md:text-4xl py-4 font-logo text-gray-800 self-center ml-4 md:ml-0">
+          CafeNearU
+        </h1>
+        <Button variant="outlined" className="my-4 text-sm mr-2 md:mr-0">
+          登出店家後台
+        </Button>
+      </div>
+    </div>
+  );
+}
