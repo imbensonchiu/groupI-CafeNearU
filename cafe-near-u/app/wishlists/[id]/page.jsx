1ff01d3a21e8e19d5471eaf8d77ea54d553e1767
IMBensonQB <b10705047@g.ntu.edu.tw>
2023-08-23 10:17:52 +0800

    fix(app/wishlist): fix case issues

--- cafe-near-u/app/wishlists/[id]/page.jsx
+++ cafe-near-u/app/wishlists/[id]/page.jsx
@@ -3,45 +3,45 @@
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
-import Header from "../../components/Header.jsx";
-import Footer from "../../components/Footer.jsx";
-import storesHome from "../../components/homepage/stores.js";
-import StoreCard from "../../components/StoreCard.jsx";
-import useWishlistitem from "../../ApiHook/userWishlistitem.jsx";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import storesHome from "../../components/homepage/stores";
+import StoreCard from "../../components/StoreCard";
+import useWishlistitem from "../../ApiHook/userWishlistitem";
 
 export default function h() {
-  const [activeButton, setActiveButton] = useState(null);
-  const name = Cookies.get("wishlistname");
+    const [activeButton, setActiveButton] = useState(null);
+    const name = Cookies.get("wishlistname");
 
-  const handleButtonClick = (buttonType) => {
-    setActiveButton(buttonType);
-  };
+    const handleButtonClick = (buttonType) => {
+        setActiveButton(buttonType);
+    };
 
-  const { userWishlistitem, fetchWishlistitem } = useWishlistitem();
+    const { userWishlistitem, fetchWishlistitem } = useWishlistitem();
 
-  useEffect(() => {
-    fetchWishlistitem();
-  }, []);
-  return (
-    <>
-      <Header />
-      <div className=" text-4xl mt-32 mb-4 ml-[10%] text-5c5c5c text-left font-bold font-normal">
-        {name}
-      </div>
-      <hr className="border-gray-300 mb-20 " />
-      <div className="container mx-auto grid grid-cols-12 gap-8 mt-8">
-        <div className="col-span-12 grid grid-cols-12 gap-8">
-          {userWishlistitem.map((store) => (
-            <StoreCard
-              className={"rounded-xl col-span-6 lg:col-span-3"}
-              key={store.id}
-              store={store}
-            />
-          ))}
-        </div>
-      </div>
+    useEffect(() => {
+        fetchWishlistitem();
+    }, []);
+    return (
+        <>
+            <Header />
+            <div className=" text-4xl mt-32 mb-4 ml-[10%] text-5c5c5c text-left font-bold font-normal">
+                {name}
+            </div>
+            <hr className="border-gray-300 mb-20 " />
+            <div className="container mx-auto grid grid-cols-12 gap-8 mt-8">
+                <div className="col-span-12 grid grid-cols-12 gap-8">
+                    {userWishlistitem.map((store) => (
+                        <StoreCard
+                            className={"rounded-xl col-span-6 lg:col-span-3"}
+                            key={store.id}
+                            store={store}
+                        />
+                    ))}
+                </div>
+            </div>
 
-      <Footer className="fixed bottom-0" />
-    </>
-  );
+            <Footer className="fixed bottom-0" />
+        </>
+    );
 }
